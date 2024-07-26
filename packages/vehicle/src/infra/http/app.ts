aa6a39ebb36db2fc9b9ee22922a078aa5ead4601
Stark <inventorstark7@gmail.com>
2024-07-26 13:40:10 +0100

    feat(vehicle): setup docs route in server

--- packages/vehicle/src/infra/http/app.ts
+++ packages/vehicle/src/infra/http/app.ts
@@ -1,24 +1,29 @@
-import cookieParser from "cookie-parser"
-import express from "express"
-import { vehicleRouterV1 } from "./api"
-import { HTTP_STATUS } from "../../../../shared/infra/http/http-status"
+import express from "express";
+import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+
+import { vehicleRouterV1 } from "./api";
+import { HTTP_STATUS } from "../../../../shared/infra/http/http-status";
+import swaggerSpec from "./docs";
 
 
-
-const app = express()
+const app = express();
 
 // set up middlewares
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
-
-app.use("/v1", vehicleRouterV1)
+// docs endpoints
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {explorer: true}));
+     
+// application routes
+app.use("/api/v1", vehicleRouterV1);
 
 app.use("*", (req, res) => {
-    res.status(HTTP_STATUS.NOT_IMPLEMENTED).json({
-        message: `${req.method} for ${req.baseUrl} not implemented`
-    })
-})
+  res.status(HTTP_STATUS.NOT_IMPLEMENTED).json({
+    message: `${req.method} for ${req.baseUrl} not implemented`,
+  });
+});
 
 export default app;

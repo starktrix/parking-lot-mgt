2b8f7a6970619622b63810a1d2194d9e8f71bd20
Stark <inventorstark7@gmail.com>
2024-07-26 13:38:34 +0100

    chore(vehicle): formatting

--- packages/vehicle/src/infra/http/api/vehicle.api.ts
+++ packages/vehicle/src/infra/http/api/vehicle.api.ts
@@ -1,24 +1,22 @@
-import Router, { Request, Response } from "express"
+import Router, { Request, Response } from "express";
 
 // import { IRequest, IResponse } from "../../../../../shared/infra/http/base"
-import { fetchVehiclesController } from "../../../services/fetch.vehicles"
-import { registerVehicleController } from "../../../services/register.vehicle"
-import { updateVehicleController } from "../../../services/update.vehicle"
+import { fetchVehiclesController } from "../../../services/fetch.vehicles";
+import { registerVehicleController } from "../../../services/register.vehicle";
+import { updateVehicleController } from "../../../services/update.vehicle";
 
-// const vehicleRouterV1 = {
-//     post(s: string, d: any){},
-//     put(s: string, d: any){},
-//     get(s: string, d: any){}
-// }
+const vehicleRouterV1 = Router();
 
-// const req = {} as IRequest
-// const res = {} as IResponse
+vehicleRouterV1.post("/vehicles/register", (req, res) =>
+  registerVehicleController.execute(req, res)
+);
 
-const vehicleRouterV1 = Router()
+vehicleRouterV1.put("/vehicles/:vehicleId", (req, res) =>
+  updateVehicleController.execute(req, res)
+);
 
-vehicleRouterV1.post("/vehicles/register", (req, res) => registerVehicleController.execute(req, res))
-vehicleRouterV1.put("/vehicles/:vehicleId", (req,res) => updateVehicleController.execute(req, res))
-vehicleRouterV1.get("/vehicles", (req, res) => fetchVehiclesController.execute(req, res))
+vehicleRouterV1.get("/vehicles", (req, res) =>
+  fetchVehiclesController.execute(req, res)
+);
 
-
-export {vehicleRouterV1}
+export { vehicleRouterV1 };
